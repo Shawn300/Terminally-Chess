f6b72172c2d4202b7125395577e82d99a11a8858
Caden McCurley <cadenmccurley@gmail.com>
2025-06-12 04:02:37 -0700

    Update Knight.java

--- Knight.java
+++ Knight.java
@@ -3,6 +3,13 @@ class Knight extends Piece {
         super(color);
     }
 
+	public boolean isValidPath(int xStart, int yStart, int xEnd, int yEnd) {
+		if (Math.abs(xStart - xEnd) == 1 && Math.abs(yStart - yEnd) == 2 || Math.abs(xStart - xEnd) == 2 && Math.abs(yStart - yEnd) == 1) {
+            return true;
+        }
+        return false;
+	}
+
     @Override
     public String toString() {
         if ("white".equals(this.color)) {
