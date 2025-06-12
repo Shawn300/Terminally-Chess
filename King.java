149858ec9b90f04b247cedd37c7a082dc88d25e0
Caden McCurley <cadenmccurley@gmail.com>
2025-06-12 04:03:03 -0700

    Update King.java

--- King.java
+++ King.java
@@ -3,6 +3,10 @@ class King extends Piece {
         super(color);
     }
 
+	public boolean isValidPath(int xStart, int yStart, int xEnd, int yEnd) {
+		return false;
+	}
+
     @Override
     public String toString() {
         if ("white".equals(this.color)) {
