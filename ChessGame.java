54d3b1f7a8e38e10a11665b3db645cbffa2f5516
Caden McCurley <cadenmccurley@gmail.com>
2025-06-12 04:01:01 -0700

    Update ChessGame.java

--- ChessGame.java
+++ ChessGame.java
@@ -1,14 +1,53 @@
 import java.util.*;
 
 public class ChessGame {
-    public static void main(String[] args) {
-	Board mainBoard = new Board();
+	private boolean endCondition = false;
+	private int turn = 0;
+
+	public ChessGame() {
+		Scanner keyboard = new Scanner(System.in);
+        Board mainBoard = new Board();
+
+		while (!endCondition) {
+			System.out.println(mainBoard);
+			if (turn % 2 == 0) {
+				System.out.println("Player 1: Enter your move (format: a 2 b 3):");
+
+				char char1 = keyboard.next().charAt(0);
+				int num1 = keyboard.nextInt();
+				char char2 = keyboard.next().charAt(0);
+				int num2 = keyboard.nextInt();
+
+				if (mainBoard.isMoveValid(char1, num1, char2, num2)) {
+					mainBoard.move(char1, num1, char2, num2);
+					System.out.println("Board after move:");
+					System.out.println(mainBoard);
+					turn++;
+				}
+
+
+			}
+			else {
+				System.out.println("Player 2: Enter your move (format: a 2 b 3):");
+
+				char char1 = keyboard.next().charAt(0);
+				int num1 = keyboard.nextInt();
+				char char2 = keyboard.next().charAt(0);
+				int num2 = keyboard.nextInt();
+
+				if (mainBoard.isMoveValid(char1, num1, char2, num2)) {
+					mainBoard.move(char1, num1, char2, num2);
+					System.out.println("Board after move:");
+					System.out.println(mainBoard);
+					turn++;
+				}
+
+
+			}
+
+		}
+	}
+	public static void main(String[] args) {
 		ChessGame game = new ChessGame();
-		System.out.println(mainBoard);
-		
-		
-		mainBoard.move('a', 1, 'a', 3);
-		
-		System.out.println(mainBoard);
-    }
+	}
 }
